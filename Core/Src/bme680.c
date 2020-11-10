2f5a71682968d9ce75c20df17074ffff8672f375
Igor Santos <igorssantos@outlook.com.br>
2020-11-10 18:04:01 -0300

    erro -2 fix (comm fail) -> erro -3 (chip id)

--- Core/Src/bme680.c
+++ Core/Src/bme680.c
@@ -295,7 +295,14 @@ int8_t user_i2c_read(void *handle, uint8_t dev_id, uint8_t reg_addr, uint8_t *re
 	 * | Stop       | -                   |
 	 * |------------+---------------------|
 	 */
-	rslt = HAL_I2C_Mem_Read( (I2C_HandleTypeDef *) handle, dev_id, reg_addr, I2C_MEMADD_SIZE_16BIT, reg_data, len, 1000);
+	rslt = HAL_I2C_Mem_Read(
+			(I2C_HandleTypeDef *) handle,		// i2c handle
+			(uint8_t) (dev_id << 1), 			// i2c address, left aligned
+			(uint8_t) reg_addr, 					// register address
+			I2C_MEMADD_SIZE_8BIT, 				// 8bit register address
+			(uint8_t*) (&reg_data),				// write returned data to this variable
+			len, 										// how many bytes to expect returned
+			1000);									// timeout
 
 	return rslt;
 }
@@ -316,7 +323,14 @@ int8_t user_i2c_write(void *handle, uint8_t dev_id, uint8_t reg_addr, uint8_t *r
 	 * | Stop       | -                   |
 	 * |------------+---------------------|
 	 */
-	rslt = HAL_I2C_Mem_Write( (I2C_HandleTypeDef *) handle, dev_id, reg_addr, I2C_MEMADD_SIZE_16BIT, reg_data, len, 1000);
+	rslt = HAL_I2C_Mem_Write(
+			(I2C_HandleTypeDef *) handle,		// i2c handle
+			(uint8_t) (dev_id << 1), 			// i2c address, left aligned
+			(uint8_t) reg_addr, 					// register address
+			I2C_MEMADD_SIZE_8BIT, 				// 8bit register address
+			(uint8_t*) (&reg_data), 			// write returned data to reg_data
+			len, 										// write how many bytes
+			1000);									// timeout
 
 	return rslt;
 }
